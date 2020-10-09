777e61e649fd997b7014030187bc4a90b2d6cae6
michcantu <70347229+michcantu@users.noreply.github.com>
2020-10-08 19:08:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 4; index++) {
+    	
+    }
+})
+for (let index = 0; index < 4; index++) {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.West)
+}
